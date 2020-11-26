3d7ddff00a95ada6c2d2dafe553dfdb788915a64
Luiz Dias <luiz@actoapp.com>
2020-11-26 13:36:12 -0500

    adding base button as custom view for a uibarbuttonitem

--- SampleProject/ViewController.m
+++ SampleProject/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) UIToolbar *toolbar;
 
 @end
 
@@ -17,9 +18,51 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [self.umItem setBackgroundImage:[UIImage imageNamed:@"Green"] forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
-    // Do any additional setup after loading the view.
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    [button setBackgroundColor:[UIColor greenColor]];
+    [button addTarget:self
+               action:@selector(acceptButtonPressed:)
+     forControlEvents:UIControlEventTouchUpInside];
+    [button setTitle:@"Accept" forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+
+    UIBarButtonItem *umItem = [[UIBarButtonItem alloc] initWithCustomView:button];
+    UIBarButtonItem *anotherItem = [[UIBarButtonItem alloc] initWithTitle:@"Another item" style:UIBarButtonItemStyleDone target:self action:@selector(acceptButtonPressed:)];
+
+    CGSize screenSize = [[UIScreen mainScreen] bounds].size;
+    CGFloat toolbarHeight = 44.0;
+    
+    self.toolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, screenSize.height-toolbarHeight, screenSize.width, toolbarHeight)];
+    
+    self.toolbar.barTintColor = [UIColor redColor];
+    UIBarButtonItem *flexItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+    [self.toolbar setItems:@[anotherItem, flexItem, umItem]];
+    
+    [self.view addSubview:self.toolbar];
+    [self.toolbar setTranslatesAutoresizingMaskIntoConstraints:NO];
+    if (@available(iOS 11.0, *)) {
+        [NSLayoutConstraint activateConstraints:@[
+            [self.toolbar.bottomAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.bottomAnchor],
+            [self.toolbar.leadingAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.leadingAnchor],
+            [self.toolbar.trailingAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.trailingAnchor],
+//            [self.toolbar.heightAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.heightAnchor],
+            [self.toolbar.heightAnchor constraintEqualToConstant:toolbarHeight],
+        ]
+         ];
+    } else {
+        // Fallback on earlier versions
+    }
+
+    [self.view addSubview:self.toolbar];
+//    UIView *targetView = (UIView *)[self.toolbar.items.firstObject performSelector:@selector(view)];
+//    CGRect firstItemFrame = targetView.frame;
+
+    CGRect frame = CGRectMake(0, 230, screenSize.width/2, toolbarHeight);
+    [[umItem customView] setFrame:frame];
+
 }
 
-
+-(void)acceptButtonPressed:(id)sender {
+    NSLog(@"nothing here");
+}
 @end
